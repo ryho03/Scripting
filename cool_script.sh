7327d81a5f6ee362dae363e6971afaa89eb3fb4a
ryho03 <rhoney95@gmail.com>
2022-11-23 14:03:03 +0000

    updating script

--- cool_script.sh
+++ cool_script.sh
@@ -1,6 +1,7 @@
 touch new_filescript.txt
 echo "Hello, hope this works" >> new_filescript.txt
 touch del_file.txt
+git add del_file.txt
 git rm del_file.txt
 mkdir script_fldr
 git mv new_filescript.txt script_fldr
