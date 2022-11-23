c0bd9844b18a59909129648a3dc4588227468005
ryho03 <rhoney95@gmail.com>
2022-11-23 13:59:36 +0000

    adding script

---
+++ cool_script.sh
@@ -0,0 +1,7 @@
+touch new_filescript.txt
+echo "Hello, hope this works" >> new_filescript.txt
+touch del_file.txt
+git rm del_file.txt
+mkdir script_fldr
+git mv new_filescript.txt script_fldr
+ls -a
